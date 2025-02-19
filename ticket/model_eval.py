9c33892f954081f9f289b40eaba277ca61605fc9
hupenghui <hupenghui>
2025-02-19 15:21:35 +0800

    revise

--- ticket/model_eval.py
+++ ticket/model_eval.py
@@ -2,7 +2,7 @@ import torch
 from torch.distributions import Categorical
 import logging,json
 import argparse
-from model_data_v2 import Model
+from model_data_v1 import Model
     
 if __name__ == '__main__':
     
@@ -11,17 +11,23 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--end", type=int, help="截断值",default=9)
     args = parser.parse_args()
     logging.info(f"当前试验截断值为：{args.end}")
+    
     model = Model()
+    
     file = '/data/hupenghui/Self/tsc/ticket/data/issue_values.json'
     with open(file, 'r', encoding='utf-8') as f:
         results = json.load(f)#无序
     all_indexs = list(results.keys())
     all_indexs = sorted(all_indexs)# 从小到大
+    
     numbers = [int(results[key]['blue']) for key in all_indexs[-100:]]
+    
     logging.info(f"原始测试数据(15个)是：{numbers[-15:]}")
+    
     test = torch.tensor(numbers).to(torch.int64)
     test -= 1
     test = test[-args.end:].unsqueeze(0)
+    
     logging.info(f"真实测试数据是：{test.flatten()}")
     
     checkpoint_path = f'/data/hupenghui/Self/tsc/ticket/model2/best_model_{args.end}.pth'
