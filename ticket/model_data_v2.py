0293f6f07b35f62f682c6dc424851e4ca2d8fcdc
hupenghui <hupenghui>
2025-02-19 15:29:13 +0800

    revise

--- ticket/model_data_v2.py
+++ ticket/model_data_v2.py
@@ -9,6 +9,7 @@ class Model(nn.Module):
     def __init__(self, output_dimension=16, max_length=51):
         super(Model, self).__init__()
         self.max_length = max_length
+        
         self.networks = nn.ModuleDict()
         for i in range(self.max_length):
             self.networks[str(i)] = nn.Sequential(
@@ -20,6 +21,31 @@ class Model(nn.Module):
                 nn.Linear(7, output_dimension),
                 nn.Tanh()
             )
+            
+        self.networks2 = nn.ModuleDict()
+        for i in range(self.max_length):
+            self.networks2[str(i)] = nn.Sequential(
+                nn.Embedding(2, 4),
+                nn.Linear(4, 5),
+                nn.LeakyReLU(),
+                nn.Linear(5, 7),
+                nn.LeakyReLU(),
+                nn.Linear(7, output_dimension),
+                nn.Tanh()
+            )
+            
+        self.networks3 = nn.ModuleDict()
+        for i in range(self.max_length):
+            self.networks3[str(i)] = nn.Sequential(
+                nn.Linear(output_dimension, 7),
+                nn.LeakyReLU(),
+                nn.Linear(7, output_dimension),
+                nn.Tanh()
+            )
+        
+        self.multihead_attn = nn.MultiheadAttention(3*output_dimension, num_heads=1)
+        self.weight = nn.Parameter(torch.randn(max_length))
+        self.output_layer = nn.Linear(3*output_dimension, 2)
         self.apply(self._init_weights)
         
     def _init_weights(self, module, gain=1.0):
@@ -29,24 +55,47 @@ class Model(nn.Module):
                 module.bias.data.zero_()
                 
     def forward(self, x):
-        batch_size, seq_length = x.shape
-        out = torch.zeros(batch_size, self.networks[str(0)][0].num_embeddings).to(x.device)  # 使用第一个网络的embedding_dim确定输出维度
+        batch_size, seq_length, features = x.shape
+        out1 = []
         for i in range(seq_length):
-            network = self.networks[str(i)]
-            out += network(x[:, i].unsqueeze(1)).squeeze(1)
-        return out
+            network = self.networks[str(i)]#4*16
+            out1.append(network(x[:, i,-2].to(torch.int64).unsqueeze(1)).squeeze(1))
+        out1 = torch.stack(out1,dim=1)
+        
+        out2 = []
+        for i in range(seq_length):
+            network = self.networks2[str(i)]#4*16
+            out2.append(network(x[:, i,-1].to(torch.int64).unsqueeze(1)).squeeze(1))
+        out2 = torch.stack(out2,dim=1)
+        
+        out3 = []
+        for i in range(seq_length):
+            network = self.networks3[str(i)]#4*16
+            out3.append(network(x[:, i,:-2].to(torch.float32)))
+        out3 = torch.stack(out3,dim=1)
+        merged_tensor = torch.cat([out1, out2, out3], dim=2)
+        
+        query = key = value = merged_tensor.permute(1, 0, 2)  # 形状变为 (5, 4, 16)
+        
+        attn_output, attn_output_weights = self.multihead_attn(query, key, value)
+        final_output = attn_output.permute(1, 0, 2)
+        
+        weights = nn.Softmax(dim=-1)(nn.Tanh()(self.weight[:seq_length]))
+        weights = weights.view(1,final_output.shape[1],1)  # 形状变为(1, 5, 1)
+        final_output = torch.sum(final_output*weights,dim=1)
+        final_output = self.output_layer(final_output)
+        
+        return final_output
 
 class MyDataset(Dataset):
-    def __init__(self, x_file = '/data/hupenghui/Self/tsc/ticket/data/train_x_5.json',y_file = '/data/hupenghui/Self/tsc/ticket/data/train_y_5.json',end =7):
+    def __init__(self, x_file = '/data/hupenghui/Self/tsc/ticket/data/train_x.json',y_file = '/data/hupenghui/Self/tsc/ticket/data/train_y.json',end = 9):
         with open(x_file, 'r', encoding='utf-8') as f:
             self.x = json.load(f)
         with open(y_file, 'r', encoding='utf-8') as f:
             self.y = json.load(f)
-        self.x = np.array(self.x,dtype=np.int64)
+        self.x = np.array(self.x,dtype=np.float32)
         self.y = np.array(self.y,dtype=np.int64)
-        self.x = self.x[:,-end:]
-        self.x -= 1
-        self.y -= 1
+        self.x = self.x[:,-end:,:]
         #print(len(self.x),len(self.x[0]),self.x[0],self.y[0])
         
     def __len__(self):
---
+++ ticket/model_train_v2.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import random_split, DataLoader
+import logging
+import argparse
+from model_data_v2 import Model,MyDataset
+
+if __name__ == '__main__':
+    
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(description="命令行参数截断值")
+    parser.add_argument("-e", "--end", type=int, help="截断值",default=9)
+    args = parser.parse_args()
+    logging.info(f"当前试验截断值为：{args.end}")
+    
+    dataset = MyDataset(end = args.end)
+    #train_size = int(0.81 * len(dataset))
+    #val_size = len(dataset) - train_size
+    val_size = 100
+    train_size = len(dataset) - val_size
+    logging.info(f"train size:{train_size},eval size:{val_size}")
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    train_loader = DataLoader(train_dataset, batch_size=train_size//3+1, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=val_size, shuffle=False)
+    logging.info(f"train groups：{len(train_loader)},eval groups：{len(val_loader)}")
+    
+    model = Model()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    num_epochs = 100000
+    best_val_loss = float('inf')  # 初始化最佳验证集损失
+    patience = 20  # 设置早停的耐心值
+    counter = 0  # 计数器，记录连续多少个epoch验证集损失没有下降
+    for epoch in range(num_epochs):
+        for i, (data, labels) in enumerate(train_loader):
+            data = data.clone().detach().to(torch.int)
+            labels = labels.clone().detach().to(torch.long) # unsqueeze(1) 增加一个维度，使其形状为 (batch_size, 1)
+            outputs = model(data)
+            loss = criterion(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i+1) % 3 == 0:
+                logging.info('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, len(train_loader), loss.item()))
+        
+        val_loss = 0.0
+        with torch.no_grad():  # 在验证阶段不需要计算梯度
+            for data, labels in val_loader:
+                data = data.clone().detach().to(torch.int)  # 验证集数据也需要转换类型
+                labels = labels.clone().detach().to(torch.long)
+                outputs = model(data)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+        avg_val_loss = val_loss / len(val_loader)
+        logging.info('Epoch [{}/{}], Validation Loss: {:.4f}'.format(epoch + 1, num_epochs, avg_val_loss))
+
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            counter = 0  # 重置计数器
+            torch.save(model.state_dict(), f'/data/hupenghui/Self/tsc/ticket/model2/best_modelv2_{args.end}.pth') # 保存当前最好的模型
+            logging.info("Model saved!")
+        else:
+            counter += 1
+            if counter >= patience:
+                logging.info('Early stopping!')
+                break  # 提前结束训练循环
+    logging.info(f"当前试验截断值为：{args.end},文件名为：/data/hupenghui/Self/tsc/ticket/model2/best_modelv2_{args.end}.pth,停止epoch:{epoch-counter},最优模型验证损失为：{best_val_loss}")
+    with open('/data/hupenghui/Self/tsc/ticket/record.txt', 'a', encoding='utf-8') as file:
+        file.write(f"当前试验截断值为：{args.end},文件名为：/data/hupenghui/Self/tsc/ticket/model2/best_modelv2_{args.end}.pth,停止epoch:{epoch-counter},最优模型验证损失为：{best_val_loss}\n")
