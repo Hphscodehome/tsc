45fab1bcdae1f4aebe16b0108f503e481228945e
hupenghui <hupenghui>
2025-02-23 20:42:03 +0800

    revise game

--- define_game.py
+++ define_game.py
@@ -26,55 +26,76 @@ class Game():
         self.recoder = defaultdict(lambda: defaultdict(lambda: deque(maxlen=self.max_length)))
         self.infos = deque(maxlen=self.max_length)
         self.state = self.world.reset()
-        self.writer = SummaryWriter(os.path.join(get_unique_log_dir(),'game'))
+        self.train_writer = SummaryWriter(os.path.join(get_unique_log_dir(),'train'))
+        self.eval_writer = SummaryWriter(os.path.join(get_unique_log_dir(),'eval'))
         self.global_step = 0
+        self.eval_results = defaultdict(list)
         
     def reset(self):
+        
         self.recoder = defaultdict(lambda: defaultdict(lambda: deque(maxlen=self.max_length)))
         self.infos = deque(maxlen=self.max_length)
         self.state = self.world.reset()
         
-    def play(self, end = 20):
-        self.recoder = defaultdict(lambda: defaultdict(lambda: deque(maxlen=self.max_length)))
-        self.infos = deque(maxlen=self.max_length)
+    def play(self, end = 20,epoch=0):
+        
+        self.reset()
         step = 0
         while step < end:
             actions,log_probs = self.world_agent.step(self.state)
             obs, rewards, dones, infos = self.world.step(actions)
             for inter in self.world.inters:
                 self.recoder[inter.id]['b_state'].append(self.state[inter.id])
-                self.writer.add_scalar(f"reward/{inter.id}", rewards[inter.id], self.global_step)
+                self.train_writer.add_scalar(f"reward_{str(epoch)}/{inter.id}", rewards[inter.id], step)
                 self.recoder[inter.id]['reward'].append(rewards[inter.id])
                 self.recoder[inter.id]['log_prob'].append(log_probs[inter.id])
                 self.recoder[inter.id]['action'].append(actions[inter.id])
                 self.recoder[inter.id]['a_state'].append(obs[inter.id])
                 self.recoder[inter.id]['done'].append(dones[inter.id])
             self.infos.append(infos)
-            for key in ['throughput','average_delay','wait_time_ascend','total_vehicles']:
-                self.writer.add_scalar(f"infos/{inter.id}/{key}", infos[inter.id][key], self.global_step)
+            for key in ['throughput','average_delay','wait_time_ascend','total_vehicles','total_wait_nums']:
+                self.train_writer.add_scalar(f"infos_{str(epoch)}/{inter.id}/{key}", infos[inter.id].dict()[key], step)
             self.state = obs
             step += 1
-            self.global_step += 1
-        #logging.info(f"Recoder:\n{self.recoder}")
-        #logging.info(f"Infos:\n{self.infos}")
+        
+    def evaluate(self,end=20,round=0):
+        
+        self.reset()
+        step = 0
+        total_reward = defaultdict(lambda: 0)
+        while step < end:
+            actions , _ = self.world_agent.eval_step(self.state)
+            eva_values = self.world_agent.eval_state(self.state)
+            obs, rewards, dones, infos = self.world.step(actions)
+            for inter in self.world.inters:
+                total_reward[inter.id] += rewards[inter.id]
+                self.eval_writer.add_scalar(f"reward_{str(round)}/{inter.id}", rewards[inter.id], step)
+                self.eval_writer.add_scalar(f"total_reward_{str(round)}/{inter.id}", total_reward[inter.id], step)
+                self.eval_writer.add_scalar(f"eval_values_{str(round)}/{inter.id}", eva_values[inter.id], step)
+            for key in ['throughput','average_delay','wait_time_ascend','total_vehicles']:
+                self.eval_writer.add_scalar(f"infos_{str(round)}/{inter.id}/{key}", infos[inter.id].dict()[key], step)
+            
+            self.state = obs
+            step += 1
+            
+        for inter in self.world.inters:
+            self.eval_results[inter.id].append(total_reward[inter.id])
+            self.eval_writer.add_scalar(f"result/{inter.id}", self.eval_results[inter.id][-1], len(self.eval_results[inter.id]))
         
     async def train(self):
         #pdb.set_trace()
         await self.world_agent.optimize(self.recoder)
-        self.recoder = defaultdict(lambda: defaultdict(lambda: deque(maxlen=self.max_length)))
-        self.infos = deque(maxlen=self.max_length)
         return True
         
 async def main():
     logging.basicConfig(level=logging.INFO)
     sumocfg = '/data/hupenghui/Self/tsc/data/syn1_1x1_1h/data.sumocfg'
     game = Game(sumocfg=sumocfg)
-    game.play(end=1000)
-    logging.info(game.infos)
-    await game.train()
-    game.play(end=1000)
-    logging.info(game.infos)
-    await game.train()
+    for i in range(20):
+        game.play(end= 1000, epoch= i)
+        logging.info(game.infos)
+        await game.train()
+        game.evaluate(end = 500, round = i)
     # pdb.set_trace()
     logging.info(f"done with: {game.cfg}")
     
